bdfeca252c57a15aec39ec29ba31bfe9b98e31ad
Soldevilla <SolDE@gmail.com>
2025-05-06 20:10:07 -0500

    5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { SecondComponent } from './pages/second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PrincipalComponent
+    },
+    {
+        path: 'category',
+        component: SecondComponent
+    }
+];
